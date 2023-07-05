cba8821b052427e380ae0c22eba11c3afd3539e3
gmartinezsenglar <gmartinez2021@alu.uct.cl>
2023-07-05 11:41:37 -0400

    Update de ventana y creados ingresos de masa y velocidad

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+#----------------------------------------------------------------------------
+#-                      Calculadora energía cinética                        -
+#----------------------------------------------------------------------------
+
 import tkinter
 
-ventana = tkinter.Tk()
-ventana.geometry('400x300')
+#----------------------------------------------------------------------------
+#-                      Constantes                                          -
+#----------------------------------------------------------------------------
 
-etiqueta = tkinter.Label(ventana, text = ' Programa - ', bg = 'blue')
+gravedad = 9.8
+res = '300x270'
+
+#----------------------------------------------------------------------------
+#-                      Creación de la ventana                              -
+#----------------------------------------------------------------------------
+
+ventana = tkinter.Tk()
+ventana.resizable(0,0)      # --> Para que usuario no pueda redimensionar la ventana
+ventana.geometry(res)       # --> Agregando las dimensiones a la ventana
+ventana.configure(background="AliceBlue")
+
+etiqueta = tkinter.Label(ventana, text = ' Calculadora de energía cinética ', font = ('Arial', 14), bg = 'cyan')
 etiqueta.pack(fill = tkinter.X)
 
-ventana.title(' Eva-Transdisciplinar-2023-D1-S2 ')
+# Etiqueta y entrada para la masa
+etiquetaMasa = tkinter.Label(ventana, text = 'Masa (kg):', font = ('Arial', 12), bg = 'AliceBlue')
+etiquetaMasa.pack()
+ingresoMasa = tkinter.Entry(ventana, font = ('Arial', 12), bg = 'AntiqueWhite2')
+ingresoMasa.pack()
+
+etiquetaVelocidad = tkinter.Label(ventana, text = 'Velocidad (kg):', font=('Arial', 12), bg = 'AliceBlue')
+etiquetaVelocidad.pack()
+ingresoVelocidad = tkinter.Entry(ventana, font=('Arial', 12), bg = 'AntiqueWhite2')
+ingresoVelocidad.pack()
+
 ventana.mainloop()
